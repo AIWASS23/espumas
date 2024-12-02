66485bce88912941e165251f92a5b8ee357ef99d
AIWASS23 <marcelodearaujo677@gmail.com>
2024-12-02 13:09:22 -0300

    Pesquisas sobre ARkit

--- Espumas/Extension.swift
+++ Espumas/Extension.swift
@@ -93,3 +93,155 @@ extension Date {
         return Self.now.timeIntervalSince1970
     }
 }
+
+extension simd_quatf {
+    func convertToEulerAngles() -> SIMD3<Float> {
+        let q = self
+        let sinr_cosp = 2 * (q.real * q.imag.z + q.imag.x * q.imag.y)
+        let cosr_cosp = 1 - 2 * (q.imag.z * q.imag.z + q.imag.x * q.imag.x)
+        
+        var roll = atan2(sinr_cosp, cosr_cosp)
+        let sinp = 2 * (q.real * q.imag.x - q.imag.y * q.imag.z)
+        
+        var pitch: Float
+        if abs(sinp) >= 1 {
+            pitch = copysign(.pi / 2, sinp)
+        } else {
+            pitch = asin(sinp)
+        }
+        
+        let siny_cosp = 2 * (q.real * q.imag.y + q.imag.z * q.imag.x)
+        let cosy_cosp = 1 - 2 * (q.imag.x * q.imag.x + q.imag.y * q.imag.y)
+
+        var yaw = atan2(siny_cosp, cosy_cosp)
+        
+        roll = roll * 180 / .pi
+        pitch = pitch * 180 / .pi
+        yaw = yaw * 180 / .pi
+
+        return SIMD3<Float>(yaw, pitch, roll)
+    }
+}
+
+extension Entity {
+    
+    func enumerateHierarchy(_ body: (Entity, UnsafeMutablePointer<Bool>) -> Void) {
+        var stop = false
+
+        func enumerate(_ body: (Entity, UnsafeMutablePointer<Bool>) -> Void) {
+            guard !stop else {
+                return
+            }
+            body(self, &stop)
+            
+            for child in children {
+                guard !stop else {
+                    break
+                }
+                child.enumerateHierarchy(body)
+            }
+        }
+        enumerate(body)
+    }
+}
+
+extension SIMD3 where Scalar == Float {
+    func distance(from other: SIMD3<Float>) -> Float {
+        return simd_distance(self, other)
+    }
+
+    var printed: String {
+        String(format: "(%.8f, %.8f, %.8f)", x, y, z)
+    }
+
+    static func spawnPoint(from: SIMD3<Float>, radius: Float) -> SIMD3<Float> {
+        from + (radius == 0 ? .zero : SIMD3<Float>.random(in: Float(-radius)..<Float(radius)))
+    }
+
+    func angle(other: SIMD3<Float>) -> Float {
+        atan2f(other.x - self.x, other.z - self.z) + Float.pi
+    }
+
+    var length: Float { return distance(from: .init()) }
+
+    var isNaN: Bool {
+        x.isNaN || y.isNaN || z.isNaN
+    }
+
+    var normalized: SIMD3<Float> {
+        return self / length
+    }
+
+    static let up: Self = .init(0, 1, 0)
+
+    func vector(to b: SIMD3<Float>) -> SIMD3<Float> {
+        b - self
+    }
+
+    var isVertical: Bool {
+        dot(self, Self.up) > 0.9
+    }
+}
+
+extension SIMD2 where Scalar == Float {
+    func distance(from other: Self) -> Float {
+        return simd_distance(self, other)
+    }
+
+    var length: Float { return distance(from: .init()) }
+}
+
+extension BoundingBox {
+
+    var volume: Float { extents.x * extents.y * extents.z }
+}
+
+
+
+extension Entity {
+    func distance(from other: Entity) -> Float {
+        transform.translation.distance(from: other.transform.translation)
+    }
+    
+    func distance(from point: SIMD3<Float>) -> Float {
+        transform.translation.distance(from: point)
+    }
+
+    func isDistanceWithinThreshold(from other: Entity, max: Float) -> Bool {
+        isDistanceWithinThreshold(from: transform.translation, max: max)
+    }
+
+    func isDistanceWithinThreshold(from point: SIMD3<Float>, max: Float) -> Bool {
+        transform.translation.distance(from: point) < max
+    }
+}
+
+extension simd_float4x4 {
+
+    var position: SIMD3<Float> {
+        SIMD3<Float>(self.columns.3.x, self.columns.3.y, self.columns.3.z)
+    }
+
+    var rotation: simd_quatf {
+        let x = simd_float3(self.columns.0.x, self.columns.0.y, self.columns.0.z)
+        let y = simd_float3(self.columns.1.x, self.columns.1.y, self.columns.1.z)
+        let z = simd_float3(self.columns.2.x, self.columns.2.y, self.columns.2.z)
+
+        let scaleX = simd_length(x)
+        let scaleY = simd_length(y)
+        let scaleZ = simd_length(z)
+
+        let sign = simd_sign(self.columns.0.x * self.columns.1.y * self.columns.2.z +
+                             self.columns.0.y * self.columns.1.z * self.columns.2.x +
+                             self.columns.0.z * self.columns.1.x * self.columns.2.y -
+                             self.columns.0.z * self.columns.1.y * self.columns.2.x -
+                             self.columns.0.y * self.columns.1.x * self.columns.2.z -
+                             self.columns.0.x * self.columns.1.z * self.columns.2.y)
+
+        let rotationMatrix = simd_float3x3(x/scaleX, y/scaleY, z/scaleZ)
+        let quaternion = simd_quaternion(rotationMatrix)
+
+        return sign >= 0 ? quaternion : -quaternion
+    }
+}
+
